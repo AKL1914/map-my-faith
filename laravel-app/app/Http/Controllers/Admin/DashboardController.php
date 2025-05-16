53a6aaf95f55128fbc1e86ba086203baad04fd03
Villamor Natonio Jr <villamornatonio@gmail.com>
2025-05-17 07:40:16 +1200

    Clean up

--- laravel-app/app/Http/Controllers/Admin/DashboardController.php
+++ laravel-app/app/Http/Controllers/Admin/DashboardController.php
@@ -3,8 +3,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 
 class DashboardController extends Controller
