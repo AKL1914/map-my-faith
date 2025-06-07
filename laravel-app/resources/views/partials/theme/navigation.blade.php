83726f52e061d42a3ebf9dcb72f9f8115ee071c9
Villamor Natonio Jr <villamornatonio@gmail.com>
2025-06-08 07:40:37 +1200

    Added fix for default sidebar hidden on mobile

--- laravel-app/resources/views/partials/theme/navigation.blade.php
+++ laravel-app/resources/views/partials/theme/navigation.blade.php
@@ -1,6 +1,6 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
+<!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center "  href="{{ url('/admin/dashboard') }}">
         <div class="sidebar-brand-icon">
             <i class="fas fa-map-marker-alt me-2"></i>
